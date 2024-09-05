564a308f6559b850f92780ef7cfdbc23b758c156
Sean W. <104464288+swilso59@users.noreply.github.com>
2024-09-05 16:20:41 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
-function insertionSortReverse(array) {
-    return array;
+function insertionSort(arr) {
+  // Change this function such that it works from the end of the array rather than the beginning
+  for(var i = arr.length-1; i >= 0; i--) {
+    var val = arr[i];
+    var j;
+    // The inner for loop needs to be adjusted.   
+    for(j = i; j < arr.length && arr[j + 1] < val; j++) {
+      arr[j] = arr[j + 1];
+    }
+    arr[j] = val;
+  }
+  return arr;
 }
